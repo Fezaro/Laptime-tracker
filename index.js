a3132ff326e46e2c221ba062931131abc35c39df
ombiro96 <bakutarb@gmail.com>
2022-12-01 23:39:05 +0300

    User interface draft

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const fs=require('fs')
 
 app.set('views', 'views');
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
 
 app.get('/', (request, response) => {
-    response.render('home', {name: 'Kirshnik Ball'});
+    response.render('home', {name: 'Fredrick Omanyala'});
     });
 app.listen(port);
 console.log('Server is running on port ' + port);
